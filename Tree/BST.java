0e7acfcbdbfdcd5e0b461ac497e22b0e4b9ffcd5
Shubham <shubhamkhanal593@gmail.com>
2024-05-31 09:17:51 +0545

    fixes

--- Tree/BST.java
+++ Tree/BST.java
@@ -26,6 +26,31 @@ public class BST {
         return root;
     }
 
+    void preOrder(Node root){
+        if (root!=null) {
+            return;
+        }
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    void inOrder(Node root){
+        if (root!=null) {
+            return;
+        }
+        preOrder(root.left);
+        System.out.println(root.data);
+        preOrder(root.right);
+    }
+    void postOrder(Node root){
+        if (root!=null) {
+            return;
+        }
+        preOrder(root.left);
+        preOrder(root.right);
+        System.out.println(root.data);
+    }
+
     Node createBST(Node root, int data){
         if (root==null){
             return new Node(data);
